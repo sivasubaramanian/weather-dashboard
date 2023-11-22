0a7190f6af6c588ad8249a4c24a75b0b10511983
sivasubramanian m <121441414+sivasubaramanian@users.noreply.github.com>
2023-11-22 20:46:24 +0530

    Add files via upload

---
+++ jass.js
@@ -0,0 +1,35 @@
+let a=document.getElementById("se");
+
+
+
+let weatherapi= async(city)=>{
+    let url='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=1bb16afdcbd193a47cf6a91a9cba556b&units=metric'
+    let we= await fetch(url);
+    let res=we.json();
+    return res;
+}
+async function callwe(){
+    let a=document.getElementById("se");
+    weatherapi(a.value)
+    .then((res)=>{
+        console.log(res);
+        let locat=res['name'];
+        let temperature=res['main']['temp']
+        let Wind1=res['wind']['speed']
+        let hum = res['main']['humidity']
+        let d=res['weather'][0]['description']
+        var locat1=document.getElementById("nameloc");
+        document.getElementById("temploc").innerHTML="Temprature: "+temperature+" °C";
+        document.getElementById("windloc").innerHTML="Wind: "+Wind1+" M/S";
+        document.getElementById("humloc").innerHTML="Humidity: "+hum+" %";
+        let daa=document.getElementById("Description");
+        daa.textContent=d;
+        locat1.textContent=locat;
+        
+
+
+    })
+
+}
+
+
